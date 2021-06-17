34298d2a0600d5954cb177e89cfd6bf52c552fcb
Sai Kumar Rayavarapu <46016311+skr2270@users.noreply.github.com>
2021-06-17 19:37:07 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,18 @@
+import json
+  
+
+with open('C:/SAI KUMAR/srujana/test.json') as json_file:
+    data = json.load(json_file)
+print("Type:", type(data))
+#print("Data:", data)
+for item in data['panels']:
+    
+    d = {'target': 1}
+    d['querylist'] = d.pop('target')
+    print(d)
+    
+
+        
+
+
+
